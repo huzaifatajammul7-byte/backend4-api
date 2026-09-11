be60fcd3e08ded2a75ee77b21fe79bb9be607b6a
huzaifatajammul7-byte <huzaifatajammul7@gmail.com>
2026-09-10 22:23:23 -0700

    Update server.js

--- server.js
+++ server.js
@@ -41,6 +41,10 @@ app.get('/',(req,res)=>{
     res.send({status:'successful',message:'Backend is working'})
 })
 
+app.post('/',(req,res)=>{
+    res.send({status:'successful',message:'Backend is working'})
+})
+
 // 1. CREATE: Naya student add karna
 app.post('/api/students', async (req, res) => {
     const { name, email, age, course } = req.body;
